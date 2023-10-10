b54f9ddbada768d0558192361eba5c8cad427b04
denervalspelizario <denervalspelizario@gmail.com>
2023-10-10 08:02:54 -0300

    update criando um service   ShowProductService para detalhar um produto especifico

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from "typeorm"; // metodo para pegar um repositorio customizado
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+import Product from "../typeorm/entities/Product";
+import AppError from "@shared/http/error/AppError";
+
+interface IResquest {
+  id: string
+}
+
+
+// classe que tem como responsabilidade Mostrar os dados de um produto
+class ShowProductService {
+  public async execute({id}: IResquest): Promise<Product | undefined>{
+
+    const productsRepository = getCustomRepository(ProductRepository); // pegando o repositorio customizado
+
+
+    // pegando um produto especifico(id) que tem em repository
+    const product = productsRepository.findOne(id);
+
+    // validando
+    if(!product){
+      throw new AppError('Product not found')
+    }
+
+    // retornando o produto
+    return product;
+  }
+}
+
+
+export default ShowProductService;
