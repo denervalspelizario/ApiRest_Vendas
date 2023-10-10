8d82b8f42fc131589c820df0526de31191a94c93
denervalspelizario <denervalspelizario@gmail.com>
2023-10-10 07:53:04 -0300

    update criando um service de listar produtos

---
+++ src/modules/products/services/ListProductService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from "typeorm"; // metodo para pegar um repositorio customizado
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+import Product from "../typeorm/entities/Product";
+
+
+// classe que tem como responsabilidade Listar os produtos
+class ListProductService {
+  public async execute(): Promise<Product[]>{
+
+    const productsRepository = getCustomRepository(ProductRepository); // pegando o repositorio customizado
+
+
+    // pegando todos os produtos que tem em repository
+    const products = productsRepository.find();
+
+    // retornando a list
+    return products;
+  }
+}
+
+
+export default ListProductService;
